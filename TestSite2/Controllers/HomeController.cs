2bf822e72e396f1233f23e131028098469880bde
ssloveca <ss.loveca18@gmail.com>
2020-02-19 14:37:56 +0800

    Third Commit

--- TestSite2/Controllers/HomeController.cs
+++ TestSite2/Controllers/HomeController.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Configuration;
+using System.Data;
+using TestSite2.Models;
 
 namespace TestSite2.Controllers
 {
     public class HomeController : Controller
     {
+        private test2dbEntities objTestdbentities;
+
+        public HomeController()
+        {
+            objTestdbentities = new test2dbEntities();
+        }
+
         public ActionResult Index()
         {
+         
             return View();
         }
 
@@ -25,6 +38,11 @@ namespace TestSite2.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult getProductlist()
+        {
+            return Json(new { data = objTestdbentities.products }, JsonRequestBehavior.AllowGet);
         }
     }
 }
